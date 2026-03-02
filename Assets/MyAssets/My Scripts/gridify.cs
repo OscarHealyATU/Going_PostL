e26c0ce5d825d6ed489b8aaedecbb0a473417be2
OscarHealyATU <G00424324@atu.ie>
2026-03-02 22:10:20 +0000

    added mountains to 2 sides of the map, and a dock to the third side of the map, added bounding box to each side so that the player cannot leave the map where sides have been added. updated the gridify script. the script now has an array to track buildings so that they can be replaced with warehouses later on. the script also has a check for a ware house to allow a starting warehouse for each zone (though in practice only one zone will start off with a warehouse).

--- Assets/MyAssets/My Scripts/gridify.cs
+++ Assets/MyAssets/My Scripts/gridify.cs
@@ -5,30 +5,42 @@ public class gridify : MonoBehaviour
     // use an array of houses instead of just one house
     // instanciate ground tile for each house
     // for buildings
+
+     [Header("Warehouse Handling")]
+    public bool hasWarehouse = false;
+    public GameObject warehousePrefab;
+    public int warehouseXPosition = 0;
+    public int warehouseZPosition = 0;
+
+
     [Header("City Seed")]
     public int seed = 42;
     public bool randomizeSeed = false;
     [SerializeField] private int previousSeed;
+
     [Header("Buildings & Props")]
     public GameObject[] housePrefabs;
     public GameObject[] streetPropPrefabs;
     public GameObject groundSquare;
+
     [Header("Grid Positioning")]
     public float xStartPosition;
     public float zStartPosition;
+
     [Header("Grid Size & Spacing")]
     public float noOfHousesX = 25f;
     public float noOfHousesZ = 25f;
     public float distance = 22f;
     public Vector3 houseScale = new Vector3(500f, 500f, 500f);
 
-
-
+   
     void Start()
     {
         previousSeed = randomizeSeed ? System.DateTime.Now.GetHashCode() : seed;
-       
         Random.InitState(previousSeed);
+        // store houses to swap them out for warehouses when purchased.
+        GameObject[,] houses = new  GameObject[(int)noOfHousesX, (int)noOfHousesZ];
+
         // tiles loop
           for (float x = 0; x < noOfHousesX; x++)
         {
@@ -51,10 +63,20 @@ public class gridify : MonoBehaviour
                 streetProp.transform.localScale = houseScale;
                 house.transform.localScale = houseScale;
                 
-               
+               houses[(int)x, (int)z] = house;
             }
         }
-        // houses loop
+        if (hasWarehouse && warehousePrefab !=null)
+        {
+            Destroy(houses[warehouseXPosition, warehouseZPosition]);
+            Destroy(houses[warehouseXPosition, warehouseZPosition]);
+            Vector3 warehousePos = new Vector3(
+                xStartPosition + warehouseXPosition * distance, 0, 
+                zStartPosition + warehouseZPosition * distance);
+
+            GameObject warehouse = Instantiate(warehousePrefab, warehousePos, Quaternion.Euler(-90, 0, 0), transform);
+            warehouse.transform.localScale = houseScale;
+        }
         
 
 
