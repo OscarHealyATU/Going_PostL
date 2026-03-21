c5bbf40890063063f0505f762f78316aff6c9777
Dylan Daly ATU <G00419061@atu.ie>
2026-03-21 14:43:08 +0000

    actually fixed broken delivery system - no jobs being created if start in main

--- Assets/DylanAssets/Script/DeliverySystem/DeliveryGridProvider.cs
+++ Assets/DylanAssets/Script/DeliverySystem/DeliveryGridProvider.cs
@@ -25,7 +25,15 @@ public class DeliveryGridProvider : MonoBehaviour
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Instance = this;
+        DontDestroyOnLoad(gameObject);
+
         BuildPoints();
     }
 
