7761aba5220102778e628142dde85af5990039bb
Dylan Daly ATU <G00419061@atu.ie>
2026-04-02 13:53:10 +0100

    fixed save point system. disabled flyover script as it was break save point rotation. added PlayerLookOutside to handle main scene camera movements

--- Assets/DylanAssets/Script/SceneFader.cs
+++ Assets/DylanAssets/Script/SceneFader.cs
@@ -2,26 +2,24 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using UnityEngine.InputSystem;
 
 public class SceneFader : MonoBehaviour
 {
     public static SceneFader Instance { get; private set; }
 
     [Header("Fade UI")]
-    public Image fadeImage;
+    [SerializeField] private Image fadeImage;
 
     [Header("Timing")]
-    public float fadeOutDuration = 0.3f;
-    public float fadeInDuration = 0.3f;
+    [SerializeField] private float fadeOutDuration = 0.35f;
+    [SerializeField] private float fadeInDuration = 0.45f;
 
-    [Header("Debug Test")]
-    public bool enableTestKey = true;
-    public string testSceneName = "Warehouse";
+    [Header("Startup")]
+    [SerializeField] private bool fadeInOnGameStart = true;
 
     private bool isFading;
 
-    void Awake()
+    private void Awake()
     {
         if (Instance != null && Instance != this)
         {
@@ -31,13 +29,26 @@ public class SceneFader : MonoBehaviour
 
         Instance = this;
 
-        SceneManager.sceneLoaded += OnSceneLoaded;
-
         EnsureFadeImage();
-        SetAlphaImmediate(0f);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
-    void OnDestroy()
+    private void Start()
+    {
+        EnsureFadeImage();
+
+        if (fadeInOnGameStart)
+        {
+            SetAlphaImmediate(1f);
+            StartCoroutine(Fade(1f, 0f, fadeInDuration));
+        }
+        else
+        {
+            SetAlphaImmediate(0f);
+        }
+    }
+
+    private void OnDestroy()
     {
         if (Instance == this)
         {
@@ -48,7 +59,9 @@ public class SceneFader : MonoBehaviour
 
     public void FadeToScene(string sceneName)
     {
-        if (isFading) return;
+        if (isFading)
+            return;
+
         StartCoroutine(FadeAndLoad(sceneName));
     }
 
@@ -57,11 +70,32 @@ public class SceneFader : MonoBehaviour
         isFading = true;
 
         EnsureFadeImage();
-        yield return Fade(0f, 1f, fadeOutDuration);
+
+        // Smooth fade to black
+        yield return Fade(fadeImage.color.a, 1f, fadeOutDuration);
 
         AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+
         while (!op.isDone)
             yield return null;
+
+        // sceneLoaded callback will handle fade-in
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        EnsureFadeImage();
+
+        // Keep screen black on first frame of new scene
+        SetAlphaImmediate(1f);
+
+        StartCoroutine(FadeInAfterSceneLoad());
+    }
+
+    private IEnumerator FadeInAfterSceneLoad()
+    {
+        // Let scene initialize one frame first
+        yield return null;
 
         EnsureFadeImage();
         yield return Fade(1f, 0f, fadeInDuration);
@@ -69,51 +103,56 @@ public class SceneFader : MonoBehaviour
         isFading = false;
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        EnsureFadeImage();
-        SetAlphaImmediate(0f);
-    }
-
     private void EnsureFadeImage()
     {
-        if (fadeImage != null) return;
+        if (fadeImage != null)
+            return;
 
-        var go = GameObject.Find("FadeImage");
+        Image[] images = GetComponentsInChildren<Image>(true);
+        foreach (var img in images)
+        {
+            if (img.name == "FadeImage")
+            {
+                fadeImage = img;
+                return;
+            }
+        }
+
+        GameObject go = GameObject.Find("FadeImage");
         if (go != null)
             fadeImage = go.GetComponent<Image>();
     }
 
-    private void SetAlphaImmediate(float a)
+    private void SetAlphaImmediate(float alpha)
     {
-        if (fadeImage == null) return;
+        if (fadeImage == null)
+            return;
 
-        var c = fadeImage.color;
-        c.a = a;
+        Color c = fadeImage.color;
+        c.a = alpha;
         fadeImage.color = c;
     }
 
     private IEnumerator Fade(float from, float to, float duration)
     {
-        if (fadeImage == null) yield break;
+        if (fadeImage == null)
+            yield break;
 
         float t = 0f;
+        Color c = fadeImage.color;
 
         while (t < duration)
         {
             t += Time.unscaledDeltaTime;
-            float progress = duration > 0 ? t / duration : 1f;
-            float alpha = Mathf.Lerp(from, to, progress);
+            float progress = duration <= 0f ? 1f : Mathf.Clamp01(t / duration);
 
-            var c = fadeImage.color;
-            c.a = alpha;
+            c.a = Mathf.Lerp(from, to, progress);
             fadeImage.color = c;
 
             yield return null;
         }
 
-        var final = fadeImage.color;
-        final.a = to;
-        fadeImage.color = final;
+        c.a = to;
+        fadeImage.color = c;
     }
 }
