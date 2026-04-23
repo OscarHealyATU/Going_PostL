e53e8dca2e1b80beffb5a8f8af69aa02a29cfd52
OscarHealyATU <G00424324@atu.ie>
2026-04-23 18:05:10 +0100

    added UI page for game over

--- Assets/OscarAssets/Oscar Scripts/playerPosReset.cs
+++ Assets/OscarAssets/Oscar Scripts/playerPosReset.cs
@@ -15,5 +15,17 @@ void OnTriggerEnter(Collider other)
            
            
         }
+         if (other.CompareTag("Vehicle"))
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player == null) return;
+           CharacterController charContrllr = other.GetComponent<CharacterController>();
+            if (charContrllr != null) charContrllr.enabled = false;
+            other.transform.position = respwanPoint.position;
+            if (charContrllr != null) charContrllr.enabled = true;
+
+           
+           
+        }
     }
 }
